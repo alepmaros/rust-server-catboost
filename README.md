0f785e33045eb3b05093331c762aef6908c9787e
Alexandre Maros <alehstk@gmail.com>
2023-04-16 20:23:04 -0300

    Simple echo server

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rust-server-catboost
 Basic rust server that runs a predict on a catboost model
+
+# TODO
+
+- [ ] Create simple server that accepts json
+- [ ] Dockerize application
+- [ ] Train simple catboost model
+- [ ] Load it and run predict on server
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use tokio::{
+    net::TcpListener,
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader}
+};
+
+#[allow(unused_variables)]
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+            
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                
+                writer.write_all(&line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
+    }
+}
