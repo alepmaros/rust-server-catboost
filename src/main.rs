ba431f60aa66eb9cebdb27340dbd7e8b80209a06
Alexandre Maros <alehstk@gmail.com>
2023-04-16 21:33:51 -0300

    Echo server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
-use std::convert::Infallible;
 use std::net::SocketAddr;
 
-use http_body_util::Full;
+use hyper::{Method, StatusCode};
+use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use tokio::net::TcpListener;
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!\n"))))
+async fn echo(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(full(
+            "Try POSTing data to /echo",
+        ))),
+        (&Method::POST, "/echo") => {
+            Ok(Response::new(req.into_body().boxed()))
+        },
+
+        // Return 404 Not Found for other routes.
+        _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
 }
 
 #[tokio::main]
@@ -22,7 +50,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
         tokio::task::spawn(async move {
             if let Err(err) = http1::Builder::new()
-                .serve_connection(stream, service_fn(hello))
+                .serve_connection(stream, service_fn(echo))
                 .await
             {
                 println!("Error serving connection: {:?}", err);
