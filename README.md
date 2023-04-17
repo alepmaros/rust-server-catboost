b3076b8b6eae1dca9be3785f26f4e141e72b8f97
Alexandre Maros <alehstk@gmail.com>
2023-04-16 21:17:36 -0300

    Moving to hyper

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Basic rust server that runs a predict on a catboost model
 - [ ] Dockerize application
 - [ ] Train simple catboost model
 - [ ] Load it and run predict on server
+
+# curl
+
+```
+curl -X POST -H "Content-Type: application/json" -d '{"key1":"value1", "key2":"value2"}' http://localhost:8080
+```
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
-use tokio::{
-    net::TcpListener,
-    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader}
-};
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
-#[allow(unused_variables)]
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use tokio::net::TcpListener;
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!\n"))))
+}
 
 #[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = TcpListener::bind(addr).await?;
 
     loop {
-        let (mut socket, addr) = listener.accept().await.unwrap();
+        let (stream, _) = listener.accept().await?;
 
-        tokio::spawn(async move {
-            let (reader, mut writer) = socket.split();
-            
-            let mut reader = BufReader::new(reader);
-            let mut line = String::new();
-
-            loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
-                }
-                
-                writer.write_all(&line.as_bytes()).await.unwrap();
-                line.clear();
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(stream, service_fn(hello))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
             }
         });
     }
